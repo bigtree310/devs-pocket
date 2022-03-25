e57aaabeb5146b02c392235c3d69257cc5ba6b93
bitree310 <ds.bigtree310@gmail.com>
2022-03-25 15:05:33 +0900

    migrate sass to css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage, InferGetStaticPropsType } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.scss'
+import styles from '../styles/Home.module.css'
 import Link from 'next/link';
 import { getPosts } from '../lib/postlib';
 
